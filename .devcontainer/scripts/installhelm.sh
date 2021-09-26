a5718b5ecac5584e153098f80ccfeb4301b82e45
yokawasa <yokawasa@gmail.com>
2021-09-26 15:48:15 +0900

    update helm installation script

--- .devcontainer/scripts/installhelm.sh
+++ .devcontainer/scripts/installhelm.sh
@@ -5,6 +5,7 @@ set -x
 # Install helm 3.0
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | DESIRED_VERSION=v3.0.3 bash
 
-# add the stable chart repo
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+# Add the stable chart repository
+# ref: https://helm.sh/docs/intro/quickstart/ 
+helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
