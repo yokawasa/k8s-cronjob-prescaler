c3febffa5063c5609c303125776ace5f6445f140
yokawasa <yokawasa@gmail.com>
2021-10-02 12:41:45 +0900

    updated

--- .devcontainer/scripts/install-kind.sh
+++ .devcontainer/scripts/install-kind.sh
@@ -5,6 +5,7 @@ set -x
 os=$(go env GOOS)
 arch=$(go env GOARCH)
 kind_version="v0.11.1"
+# ref: https://kind.sigs.k8s.io/docs/user/quick-start/#installation
 
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/${kind_version}/kind-${os}-${arch}
 chmod +x ./kind
